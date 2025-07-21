a81593975005d3aabe8bf87000a3dc428a8162a9
ha5804 <zixpotf5803@gmail.com>
2025-07-21 15:31:34 +0900

    write code

--- a.py
+++ a.py
@@ -32,26 +32,76 @@ class activation:
         pass
 
     def relu(self, x):
-        return np.maximun(0, x)
+        return np.maximum(0, x)
     def softmax(self, x):
         e_x = np.exp(x - np.max(x, axis = 1, keepdims = True))
         return e_x / np.sum(e_x, axis = 1, keepdims = True)
     
-    def cross_entropy(self, pred, label):
-        n = pred.shape[0]
-        log_likelihood = -np.log(pred[range(n), label])
+    def cross_entropy(self, y_hat, label):
+        n = y_hat.shape[0]
+        log_likelihood = -np.log(y_hat[range(n), label])
         return np.sum(log_likelihood) / n
     
-class MLP(activation):
-    def __init__(self, input_size, hidden_size, num_label = 4):
-        self.w = np.zeros((input_size, hidden_size))
-        self.b = np.zeros((1, hidden_size))
-        self.w_2 = np.zeros((hidden_size, num_label))
-        self.b_2 = np.zeros((1, num_label))
-    
-    def forward(self, x):
-        y = self.w @ x + self.b
-        A1 = self.relu(y)
-        z = self.w_2 @ A1 + self.b_2 
+import numpy as np
 
-    
+# Activation + Loss
+class Activation:
+    def relu(self, x):
+        return np.maximum(0, x)
+
+    def softmax(self, x):
+        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return e_x / np.sum(e_x, axis=1, keepdims=True)
+
+    def cross_entropy(self, y_hat, label):
+        label = label.reshape(-1)
+        n = y_hat.shape[0]
+        log_likelihood = -np.log(y_hat[range(n), label])
+        return np.sum(log_likelihood) / n
+
+# MLP 신경망
+class MLP(Activation):
+    def __init__(self, input_size, hidden_size, num_label=4):
+        super().__init__()
+        self.w = np.random.randn(input_size, hidden_size) * 0.01
+        self.b = np.zeros((1, hidden_size))
+        self.w_2 = np.random.randn(hidden_size, num_label) * 0.01
+        self.b_2 = np.zeros((1, num_label))
+        self.learning_rate = 0.1
+
+    def forward(self, x):
+        self.Z1 = x @ self.w + self.b
+        self.A1 = self.relu(self.Z1)
+        self.Z2 = self.A1 @ self.w_2 + self.b_2
+        self.A2 = self.softmax(self.Z2)
+        return self.A2
+
+    def backward(self, x, label):
+        n = x.shape[0]
+        dZ2 = self.A2.copy()
+        dZ2[range(n), label.reshape(-1)] -= 1
+        dZ2 /= n
+
+        dW2 = self.A1.T @ dZ2
+        db2 = np.sum(dZ2, axis=0, keepdims=True)
+
+        dA1 = dZ2 @ self.w_2.T
+        dZ1 = dA1 * (self.Z1 > 0)
+
+        dW1 = x.T @ dZ1
+        db1 = np.sum(dZ1, axis=0, keepdims=True)
+
+        self.w -= self.learning_rate * dW1
+        self.b -= self.learning_rate * db1
+        self.w_2 -= self.learning_rate * dW2
+        self.b_2 -= self.learning_rate * db2
+
+    def train(self, x, label):
+        y_hat = self.forward(x)
+        loss = self.cross_entropy(y_hat, label)
+        self.backward(x, label)
+        return loss
+
+    def predict(self, x):
+        probs = self.forward(x)
+        return np.argmax(probs, axis=1)
